1e8e105c0d008949301f9c9cbdb8cac870c63331
Vlada1234 <zivkovicv107@gmail.com>
2024-02-02 09:42:47 +0100

    added docker-compose.yml

--- src/main/java/com/urlshortener/demo/UrlshortenerApplication.java
+++ src/main/java/com/urlshortener/demo/UrlshortenerApplication.java
@@ -2,6 +2,7 @@ package com.urlshortener.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class UrlshortenerApplication {
